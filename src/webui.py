7490a2af83a10c0d2f163c6f0c112a2d976691e0
FreyzaMarshall <134063936+freyzamarshall02@users.noreply.github.com>
2025-12-23 23:28:24 +0700

    Update song input info to include Soundcloud and Spotify

--- src/webui.py
+++ src/webui.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
                         ref_btn = gr.Button('Refresh Models 🔁', variant='primary')
 
                     with gr.Column() as yt_link_col:
-                        song_input = gr.Text(label='Song input', info='Link to a song on YouTube or full path to a local file. For file upload, click the button below.')
+                        song_input = gr.Text(label='Song input', info='Link to a song on YouTube, Soundcloud and Spotify or full path to a local file. For file upload, click the button below.')
                         show_file_upload_button = gr.Button('Upload file instead')
 
                     with gr.Column(visible=False) as file_upload_col:
